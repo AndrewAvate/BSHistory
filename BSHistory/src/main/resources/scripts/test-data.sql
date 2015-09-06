9d522139b73642d16fa09b548698e1f841038ca0
Andrew Avate <3642542@gmail.com>
2015-09-06 14:08:58 +0500

    remove unnecessary directions

--- BSHistory/src/main/resources/scripts/test-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO base_station (bs_title, create_date) VALUES ('EK_SWT', '2015-07-30');
-INSERT INTO base_station (bs_title, create_date) VALUES ('EK_EKT', '2014-11-02');
-INSERT INTO base_station (bs_title, create_date) VALUES ('EK_TRE', '2014-02-28');
-INSERT INTO base_station (bs_title, create_date) VALUES ('EK_MTT', '2015-1-10');
